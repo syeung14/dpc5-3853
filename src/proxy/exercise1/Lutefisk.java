a206d830dd3ecc3a92833e3b607f89db235762eb
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2022-12-15 19:40:23 +0200

    proxy exercise 1 done

--- src/proxy/exercise1/Lutefisk.java
+++ src/proxy/exercise1/Lutefisk.java
@@ -3,16 +3,11 @@
  * Dr Heinz Kabutz from JavaSpecialists.eu and may not be
  * distributed without written consent.
  *
- * Copyright 2001-2018, Heinz Kabutz, All rights reserved.
+ * Copyright 2001-2022, Heinz Kabutz, All rights reserved.
  */
+
 package proxy.exercise1;
 
-public class Lutefisk {
-    public Lutefisk() {
-        System.out.println("Lutefisk created!");
-    }
-
-    public void eat() {
-        System.out.println("You will regret eating me!");
-    }
+public interface Lutefisk {
+    void eat();
 }
